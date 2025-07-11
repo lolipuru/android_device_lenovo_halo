a163b44060fedfb8ea7ca9411cd41fb05999e1d7
lolipuru <banstellazz@gmail.com>
2025-07-11 07:33:39 +0700

    halo: move sepolicy to common
    let common handle it for both halo and asphalt, this commit also revert 1b86d90c0d887fffa097f20d43198bcec16a65fc as well
    
    Change-Id: I083b6916db6b8213c0ea2976152d7568371c3a17
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,10 +31,5 @@ PRODUCT_COPY_FILES += \
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
-# Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # Inherit from the proprietary version
 include vendor/lenovo/halo/BoardConfigVendor.mk
